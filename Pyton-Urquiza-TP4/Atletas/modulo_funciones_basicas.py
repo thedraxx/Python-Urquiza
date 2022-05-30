c1ea50db2e9512b301696c43310782301701577f
Frank <55867157+thedraxx@users.noreply.github.com>
2022-05-30 14:07:06 -0300

    Add files via upload

---
+++ Pyton-Urquiza-TP4/Atletas/modulo_funciones_basicas.py
@@ -0,0 +1,57 @@
+
+                  
+
+def leer_entero_rango(cartel:str, desde:int, hasta:int) -> int:
+
+
+    todo_bien = False
+    while not todo_bien:
+        try:
+            entero = int(input(f"{cartel}"))
+        except:
+            pass
+        else:
+            if desde <= entero <= hasta:
+                todo_bien = True
+    return entero
+
+def leer_entero_rango_union(cartel:str, desde:int, hasta:int, union:int) -> int:
+
+    todo_bien = False
+    while not todo_bien:
+        try:
+            entero = int(input(f"{cartel}"))
+        except:
+            pass
+        else:
+            if (desde <= entero <= hasta) or (entero == union):
+                todo_bien = True
+    return entero
+
+                                # FUNCIONES CON FLOAT O VALORES DE PUNTO FLOTANTE
+
+def leer_float_rango(cartel:str, desde:float, hasta:float) -> float:
+
+    todo_bien = False
+    while not todo_bien:
+        try:
+            valor_float = float(input(f"{cartel}"))
+        except:
+            pass
+        else:
+            if desde <= valor_float <= hasta:
+                todo_bien = True
+    return valor_float
+
+def leer_float_rango_union(cartel:str, desde:float, hasta:float, union:float) -> float:
+
+    todo_bien = False
+    while not todo_bien:
+        try:
+            valor_float = float(input(f"{cartel}"))
+        except:
+            pass
+        else:
+            if (desde <= valor_float <= hasta) or (valor_float == union):
+                todo_bien = True
+    return valor_float
