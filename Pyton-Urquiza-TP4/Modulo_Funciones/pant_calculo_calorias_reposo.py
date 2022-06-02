5151071d7168a697f3dafcd4efb359d4728f8a79
Frank <55867157+thedraxx@users.noreply.github.com>
2022-06-02 16:22:09 -0300

    Delete Pyton-Urquiza-TP4/Modulo_Funciones directory

--- Pyton-Urquiza-TP4/Modulo_Funciones/pant_calculo_calorias_reposo.py
+++
@@ -1,6 +0,0 @@
-from calculadora_indices import peso, altura, genero, edad
-from TmbApp import calcular_calorias_en_reposo
-
-# Tasa metabólica basal
-TMB = calcular_calorias_en_reposo(peso, altura, edad, genero)
-print('Valor Tasa metabólica basal: ', TMB)
