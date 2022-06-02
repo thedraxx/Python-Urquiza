135665203fc12e49dbaae88ee40e3a680e51b37a
Frank <55867157+thedraxx@users.noreply.github.com>
2022-06-02 15:48:53 -0300

    Delete Pyton-Urquiza-TP4/Atletas directory

--- Pyton-Urquiza-TP4/Atletas/Prueba_MasTiempo.py
+++
@@ -1,3 +0,0 @@
-def maxT(tiempo_prueba_lista: list) -> bool:
-    mayor = max(tiempo_prueba_lista)
-    return print("El mayor tiempo que hizo el atleta fue de: ", mayor)
--- Pyton-Urquiza-TP4/Atletas/Prueba_MenosTiempo.py
+++
@@ -1,3 +0,0 @@
-def minT(tiempo_prueba_lista: list) -> bool:
-    menor = min(tiempo_prueba_lista)
-    return print("El menor tiempo que hizo el atleta fue de: ", menor)
